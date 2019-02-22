cc13c528a11cb87b27bf265ea25831a6619dbfa6
Toby New <toby.new@newitsolutions.co.uk>
2019-02-22 02:46:08 +0000

    Adding in missed reference

--- src/apicore/structure/response.php
+++ src/apicore/structure/response.php
@@ -82,7 +82,7 @@ class response{
     public function xml2array ( $xmlObject, $out = array () )
     {
         foreach ( (array) $xmlObject as $index => $node )
-            $out[$index] = ( is_object ( $node ) ||  is_array ( $node ) ) ? xml2array ( $node ) : $node;
+            $out[$index] = ( is_object ( $node ) ||  is_array ( $node ) ) ? $this->xml2array ( $node ) : $node;
 
         return $out;
     }
